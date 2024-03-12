3e2d3d6b52636c7a7fa327b70abd2849e3454332
Gennahockin <124071360+Gennahockin@users.noreply.github.com>
2024-03-11 21:06:43 -0600

    Pojo for Student.java
    I couldn't get it on my eclipse but I edited on git hub so pray it works

--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -1,5 +1,74 @@
 
 public class Student
 	{
-		
+		private String firstName;
+		private String lastName; 
+		private double gpa;
+		private int algGrade;
+		private int engGrade;
+		private int chemGrade;
+
+		public Student(String firstName; String lastName; double gpa; int algGrade; int engGrade; int chemGrade I )
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.gpa = gpa;
+			this.algGrade = algGrade;
+			this.engGrade = endGrade;
+			this.chemGrade = chemGrade;
+		}
+		//first name
+		public String getFirstName()
+		{
+			return firstName;
+		}
+		public void setFirstName()
+		{
+			this.firstName = firstName;
+		}
+		//lastname
+		public String getLastName()
+		{
+			return lastName;
+		}
+		public void setLastName()
+		{
+			this.lastName = lastName;
+		}
+		//gpa
+		public double getGpa()
+		{
+			return gpa;
+		}
+		public void setGpa()
+		{
+			this.gpa = gpa;
+		}
+		//algebra grade
+		public int getAlgGrade()
+		{
+			return algGrade;
+		}
+		public void setAlgGrade()
+		{
+			this.algGrade = algGrade;
+		}
+		//english grade
+		public int getEngGrade()
+		{
+			return engGrade;
+		}
+		public void setEngGrade()
+		{
+			this.engGrade = engGrade;
+		}
+		//chem grade
+		public int getChemGrade()
+		{
+			return chemGrade;
+		}
+		public void setChemGrade()
+		{
+			this.chemGrade = chemGrade;
+		}
 	}
